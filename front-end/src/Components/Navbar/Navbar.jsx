7c9e0a70d0d618bc32bed2f0143ca33425f5f2c0
Rauber Simoes do Nascimento <raubersn@gmail.com>
2024-04-04 20:34:09 -0700

    Set state on the Menu

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
+
 export const Navbar = () => {
+
+  const [menu, setMenu] = useState('shop');
+
   return (
     <div className='navbar'>
       <div className='nav-logo'>
@@ -11,10 +15,10 @@ export const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className='nav-menu'>
-        <li>Shop<hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => setMenu('shop')}>Shop{menu==='shop'&&<hr/>}</li>
+        <li onClick={() => setMenu('men')}>Men{menu==='men'&&<hr/>}</li>
+        <li onClick={() => setMenu('women')}>Women{menu==='women'&&<hr/>}</li>
+        <li onClick={() => setMenu('kids')}>Kids{menu==='kids'&&<hr/>}</li>
       </ul>
       <div className='nav-login-cart'>
         <button>Login</button>
